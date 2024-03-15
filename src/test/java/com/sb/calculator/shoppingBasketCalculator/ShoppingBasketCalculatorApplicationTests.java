61084e7e1f256ae20cb659c6e1de4197d3c83ee2
Sunny Anand <sunny@192.168.1.120>
2024-03-15 01:00:24 +0000

    Initial commit

---
+++ src/test/java/com/sb/calculator/shoppingBasketCalculator/ShoppingBasketCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.calculator.shoppingBasketCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBasketCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
